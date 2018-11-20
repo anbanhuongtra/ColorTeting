33f825a496c5b9a5455c47da692343ed3ea58afd
mtienbui97 <15520870@gm.uit.edu.vn>
2018-11-19 18:45:32 -0800

    Add files via upload

---
+++ game-chon-mau-master/game-chon-mau-master/GameChonMau/GameChonMau/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace GameChonMau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 87);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 49);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 87);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 49);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 87);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 49);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button4.Location = new System.Drawing.Point(43, 141);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 49);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(92, 141);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 49);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(142, 141);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(45, 49);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(43, 194);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(45, 49);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(92, 194);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(45, 49);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(142, 194);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(45, 49);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Point";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(354, 55);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(300, 300);
+            this.flowLayoutPanel1.TabIndex = 11;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 419);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ game-chon-mau-master/game-chon-mau-master/GameChonMau/GameChonMau/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameChonMau
+{
+    public partial class Form1 : Form
+    {
+        int k; // đánh dấu
+        int point = 0; // điểm đạt được
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SetColor();
+            RenderGrid();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (k == 4) {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+        public void SetColor()
+        {
+            Random p = new Random();
+            
+            int x = p.Next(0, 255);
+            int y = p.Next(0, 255);         // màu ngẫu nhiên
+            int z = p.Next(30, 255);
+
+            Random q = new Random();
+            k = q.Next(1,9);        // ô ngẫu nhiên khác màu
+           
+            if (k == 1)
+            {
+                button1.BackColor = Color.FromArgb(x, y, z - 30);
+           
+            }
+            else button1.BackColor = Color.FromArgb(x, y, z);
+            if (k == 2)
+            {
+                button2.BackColor = Color.FromArgb(x, y, z - 30);
+                
+            }
+            else button2.BackColor = Color.FromArgb(x, y, z);
+            if (k == 3)
+            {
+                button3.BackColor = Color.FromArgb(x, y, z - 30);
+              
+            }
+            else button3.BackColor = Color.FromArgb(x, y, z);
+            if (k == 4)
+            {
+                button4.BackColor = Color.FromArgb(x, y, z - 30);
+                
+            }
+            else button4.BackColor = Color.FromArgb(x, y, z);
+            if (k == 5)
+            {
+                button5.BackColor = Color.FromArgb(x, y, z - 30);
+               
+            }
+            else button5.BackColor = Color.FromArgb(x, y, z);
+            if (k == 6)
+            {
+                button6.BackColor = Color.FromArgb(x, y, z - 30);
+            }
+
+            else button6.BackColor = Color.FromArgb(x, y, z);
+            if (k == 7)
+            {
+                button7.BackColor = Color.FromArgb(x, y, z - 30);
+                
+            }
+            else button7.BackColor = Color.FromArgb(x, y, z);
+            if (k == 8)
+            {
+                button8.BackColor = Color.FromArgb(x, y, z - 30);
+              
+            }
+            else button8.BackColor = Color.FromArgb(x, y, z);
+            if (k == 9)
+            {
+                button9.BackColor = Color.FromArgb(x, y, z - 30);
+               
+            }
+            else button9.BackColor = Color.FromArgb(x, y, z);
+        }
+
+        public int GetGridLevel(int level)
+        {
+            if (level < 2) return 2;
+            if (level < 4) return 3;
+            if (level < 8) return 4;
+            if (level < 13) return 5;
+            if (level < 22) return 6;
+            if (level < 32) return 7;
+            if (level < 36) return 8;
+            if (level < 40) return 9;
+            if (level < 44) return 10;
+            if (level < 48) return 11;
+            return 12;
+        }
+        int level = 1;
+        int correct_tile = -1;
+
+        Random color_random = new Random();
+        Random tile_random = new Random();
+
+        public void RenderGrid()
+        {
+            int grid = GetGridLevel(level);
+
+            //set position of correct tile
+            int position = -1;
+            do
+            {
+                position = tile_random.Next(0, grid * grid -1);
+            } while (correct_tile == position);
+
+            //set color 
+            var colordiff = getLevelColorDiff(level);
+            var r = color_random.Next(0, (255 - colordiff));
+            var g = color_random.Next(0, (255 - colordiff));
+            var b = color_random.Next(0, (255 - colordiff));
+
+            //render grid
+            for (int i = 0; i < grid * grid; i++)
+            {
+                Button tile = new Button();
+                tile.Width = (flowLayoutPanel1.Width / grid) - 10; // error at higher level
+                tile.Height = (flowLayoutPanel1.Height / grid) - 10; // error at higher level
+                tile.Tag = i;
+                
+                if (i != position)
+                {
+                    tile.BackColor = Color.FromArgb(255, r, g, b);
+                    tile.Click += tile_Click_miss;
+                }
+                else
+                {
+                    tile.BackColor = Color.FromArgb(255, r+colordiff, g+colordiff, b+colordiff);
+                    tile.Click += tile_Click_correct;
+                }
+                flowLayoutPanel1.Controls.Add(tile);
+            }
+
+            
+
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (k == 5)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (k == 1)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (k == 2)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (k == 3)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (k == 6)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (k == 7)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (k == 8)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (k == 9)
+            {
+                point++;
+                label1.Text = point.ToString();
+                SetColor();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        //tile click event
+        void tile_Click_correct(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            level++;
+            flowLayoutPanel1.Controls.Clear();
+            RenderGrid();
+
+        }
+
+        void tile_Click_miss(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            
+
+        }
+
+
+        //  set difficult level
+        int[] col = { 105, 75, 60, 45, 30, 20, 18, 16, 15, 15, 15, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1 };
+        public int getLevelColorDiff(int level)
+        {
+            if (level <= 58)
+            {
+                return col[level - 1];
+            }
+            return 1;
+        }
+
+    }
+}
